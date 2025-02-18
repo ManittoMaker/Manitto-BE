54c4562c75a962712108cbd60126249e46081465
ajroot5685 <anjeong8577@naver.com>
2025-02-18 17:16:35 +0900

    ✨ Feat: 스웨거 추가, 규격화된 응답 dto 추가
    #1

---
+++ src/main/java/manitto/backend/global/config/swagger/SwaggerResponseDescription.java
@@ -0,0 +1,33 @@
+package manitto.backend.global.config.swagger;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+import lombok.Getter;
+import manitto.backend.global.exception.ErrorCode;
+
+@Getter
+public enum SwaggerResponseDescription {
+
+    EXAMPLE(new LinkedHashSet<>(Set.of(
+            ErrorCode.SERVER_UNTRACKED_ERROR
+    ))),
+    ;
+
+    private Set<ErrorCode> errorCodeList;
+
+    SwaggerResponseDescription(Set<ErrorCode> errorCodeList) {
+        // 공통 에러
+        errorCodeList.addAll(new LinkedHashSet<>(Set.of(
+                ErrorCode.SERVER_UNTRACKED_ERROR,
+                ErrorCode.INVALID_PARAMETER,
+                ErrorCode.PARAMETER_VALIDATION_ERROR,
+                ErrorCode.PARAMETER_GRAMMAR_ERROR,
+                ErrorCode.UNAUTHORIZED,
+                ErrorCode.FORBIDDEN,
+                ErrorCode.AUTHORIZED_ERROR,
+                ErrorCode.OBJECT_NOT_FOUND
+        )));
+
+        this.errorCodeList = errorCodeList;
+    }
+}
