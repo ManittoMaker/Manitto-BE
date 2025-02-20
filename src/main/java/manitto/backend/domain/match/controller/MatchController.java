8863bd5ce926a777489fcf81fdaafb3947dbcdeb
ajroot5685 <anjeong8577@naver.com>
2025-02-20 18:44:27 +0900

    ✨ Feat: 매칭 클래스 간 연관관계 설정
    #12

--- src/main/java/manitto/backend/domain/match/controller/MatchController.java
+++ src/main/java/manitto/backend/domain/match/controller/MatchController.java
@@ -1,4 +1,18 @@
 package manitto.backend.domain.match.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import manitto.backend.domain.match.service.MatchService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "매칭 관련 API")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/match")
 public class MatchController {
+
+    private final MatchService matchService;
+
+    
 }
--- src/main/java/manitto/backend/domain/match/service/MatchService.java
+++ src/main/java/manitto/backend/domain/match/service/MatchService.java
@@ -1,4 +1,12 @@
 package manitto.backend.domain.match.service;
 
+import lombok.RequiredArgsConstructor;
+import manitto.backend.domain.match.repository.MatchRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class MatchService {
+
+    private final MatchRepository matchRepository;
 }
