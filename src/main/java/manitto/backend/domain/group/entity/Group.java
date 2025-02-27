69defa63deb608621376597222bec7d143e681d8
ajroot5685 <anjeong8577@naver.com>
2025-02-27 19:08:08 +0900

    ✨ Feat: Group 엔티티 생성

---
+++ src/main/java/manitto/backend/domain/group/entity/Group.java
@@ -0,0 +1,34 @@
+package manitto.backend.domain.group.entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import manitto.backend.global.entity.BaseEntity;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "groups")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Group extends BaseEntity {
+
+    @Id
+    private String id;
+    private String leaderName;
+    private String groupName;
+    private String password;
+
+
+    public static Group create(String leaderName, String groupName, String password) {
+        return Group.builder()
+                .id(generateFirestoreId())
+                .leaderName(leaderName)
+                .groupName(groupName)
+                .password(password)
+                .build();
+    }
+}
