8082944ac0f1c21121c8c726a7a1d478ab5bb2bd
ajroot5685 <anjeong8577@naver.com>
2025-02-27 19:11:01 +0900

    ✨ Feat: Group 기본 레포지토리 생성

---
+++ src/main/java/manitto/backend/domain/group/repository/GroupRepository.java
@@ -0,0 +1,8 @@
+package manitto.backend.domain.group.repository;
+
+import manitto.backend.domain.group.entity.Group;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface GroupRepository extends MongoRepository<Group, String> {
+
+}
