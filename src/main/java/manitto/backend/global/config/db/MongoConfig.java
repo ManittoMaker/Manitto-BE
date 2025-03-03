1d585abf81b75ed8aaf8641fff3adac020653134
ajroot5685 <anjeong8577@naver.com>
2025-03-03 17:22:42 +0900

    ✨ Feat: 엔티티 createdAt 오류 수정 및 TTL 설정
    createdAt 필드가 생성되지 않은 문제가 있었다
    이는 Id를 직접 생성했기 때문으로 Persistable의 isNew()메서드를 오버라이딩하여 해결한다
    
    TTL을 한달로 설정한다

---
+++ src/main/java/manitto/backend/global/config/db/MongoConfig.java
@@ -0,0 +1,9 @@
+package manitto.backend.global.config.db;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {
+}
