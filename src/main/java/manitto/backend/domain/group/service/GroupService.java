d7cb76704f5ee2667d8c7c7bdfdef07698b6e141
mingdodev <mingdo.dev@gmail.com>
2025-03-04 11:33:17 +0900

    ✅ Test(Group): create() GREEN
    테스트를 통과하는 최소한의 코드를 추가
    
    #15

--- src/main/java/manitto/backend/domain/group/service/GroupService.java
+++ src/main/java/manitto/backend/domain/group/service/GroupService.java
@@ -1,9 +1,12 @@
 package manitto.backend.domain.group.service;
 
 import lombok.RequiredArgsConstructor;
+import manitto.backend.domain.group.dto.GroupDtoMapper.GroupDtoMapper;
 import manitto.backend.domain.group.dto.request.GroupCreateReq;
 import manitto.backend.domain.group.dto.response.GroupCreateRes;
+import manitto.backend.domain.group.entity.Group;
 import manitto.backend.domain.group.repository.GroupRepository;
+import manitto.backend.global.util.PasswordProvider;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,7 +17,14 @@ public class GroupService {
 
     public GroupCreateRes create(GroupCreateReq req) {
 
-        return null;
+        String leaderName = req.getLeaderName();
+        String groupName = req.getGroupName();
+        String password = PasswordProvider.generatePassword();
+
+        Group group = Group.create(leaderName, groupName, password);
+        group = groupRepository.save(group);
+
+        return GroupDtoMapper.toGroupCreateRes(group.getId());
     }
 
 }
