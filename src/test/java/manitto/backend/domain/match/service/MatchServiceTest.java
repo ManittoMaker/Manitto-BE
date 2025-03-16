1c5872e3e45030203d3ca3f93e8b2030de1e5ad2
mingdodev <mingdo.dev@gmail.com>
2025-03-16 22:42:03 +0900

    ✅ Test(Match): getGroupResult() RED
    - 기존 마니또 그룹 결과 조회를 위해 Group, Match를 가져오는 API test
    
    #25

--- src/test/java/manitto/backend/domain/match/service/MatchServiceTest.java
+++ src/test/java/manitto/backend/domain/match/service/MatchServiceTest.java
@@ -2,6 +2,7 @@ package manitto.backend.domain.match.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -90,5 +91,43 @@ class MatchServiceTest {
                 .collect(Collectors.toMap(MatchResult::getGiver, MatchResult::getReceiver));
         assertThat(matchMap).containsEntry(member1, member2);
         assertThat(matchMap).containsEntry(member2, member1);
+    }
+
+    @Test
+    void getGroupResults_정상_응답() {
+        // given
+        String leaderName = "리더명";
+        String groupName = "그룹명";
+        String password = "그룹 비밀번호";
+        Group group = Group.create(leaderName, groupName, password);
+        group = groupRepository.save(group);
+
+        List<MatchResult> matches = new ArrayList<>(List.of(
+                MatchResult.create("멤버1", "멤버1 비밀번호", "멤버2"),
+                MatchResult.create("멤버2", "멤버2 비밀번호", "멤버3"),
+                MatchResult.create("멤버3", "멤버3 비밀번호", "멤버1")
+        ));
+        Match match = Match.create(group.getId(), matches);
+        match = matchRepository.save(match);
+
+        MatchGetGroupResultReq req = MatchDtoMother.createMatchGetGroupResultReq(leaderName, groupName, password);
+
+        // when
+        MatchGetGroupResultRes result = matchService.getGroupResult(req);
+
+        // then
+        assertThat(result.getGroup()).isNotNull;
+        assertThat(result.getMatch()).isNotNull;
+
+        assertThat(result.getGroup().getGroupId()).isEqualTo(group.getId());
+        assertThat(result.getMatch().getGroupId()).isEqualTo(group.getId());
+
+        List<MatchResult> matchesResult = result.getMatch().getMatches();
+
+        for (int i = 0; i < matchesResult.size(); i++) {
+            assertThat(matchesResult.get(i).getGiver()).isEqualTo(matches.get(i).getGiver());
+            assertThat(matchesResult.get(i).getPassword()).isEqualTo(matches.get(i).getPassword());
+            assertThat(matchesResult.get(i).getReceiver()).isEqualTo(matches.get(i).getReceiver());
+        }
     }
 }
