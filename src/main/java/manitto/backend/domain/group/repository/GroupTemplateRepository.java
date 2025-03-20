31c3af68d691161e792d401de123693e470b939e
mingdodev <mingdo.dev@gmail.com>
2025-03-20 23:04:33 +0900

    ✨ Feat: 그룹 생성 시 카운트 증가 테스트 GREEN
    #29

--- src/main/java/manitto/backend/domain/group/repository/GroupTemplateRepository.java
+++ src/main/java/manitto/backend/domain/group/repository/GroupTemplateRepository.java
@@ -3,11 +3,15 @@ package manitto.backend.domain.group.repository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import manitto.backend.domain.group.entity.Group;
+import manitto.backend.domain.group.entity.GroupCount;
 import manitto.backend.global.exception.CustomException;
 import manitto.backend.global.exception.ErrorCode;
+import manitto.backend.global.repository.GlobalMongoTemplateRepository;
+import org.springframework.data.mongodb.core.FindAndModifyOptions;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -15,6 +19,21 @@ import org.springframework.stereotype.Repository;
 public class GroupTemplateRepository {
 
     private final MongoTemplate mongoTemplate;
+    private final GlobalMongoTemplateRepository globalMongoTemplateRepository;
+
+    public Group create(String leaderName, String groupName, String password) {
+
+        Group group = Group.create(leaderName, groupName, password);
+        globalMongoTemplateRepository.saveWithoutDuplicatedId(group, Group.class);
+
+        Query query = new Query(Criteria.where("id").is("groupCount"));
+        Update update = new Update().inc("totalGroups", 1);
+        FindAndModifyOptions options = new FindAndModifyOptions().returnNew(true).upsert(true);
+
+        mongoTemplate.findAndModify(query, update, options, GroupCount.class);
+
+        return group;
+    }
 
     public Group findGroupByLeaderNameAndGroupNameAndPassword(String leaderName, String groupName, String password) {
         Query query = new Query();
--- src/main/java/manitto/backend/domain/group/service/GroupService.java
+++ src/main/java/manitto/backend/domain/group/service/GroupService.java
@@ -7,7 +7,7 @@ import manitto.backend.domain.group.dto.response.GroupCountRes;
 import manitto.backend.domain.group.dto.response.GroupCreateRes;
 import manitto.backend.domain.group.entity.Group;
 import manitto.backend.domain.group.repository.GroupCountTemplateRepository;
-import manitto.backend.global.repository.GlobalMongoTemplateRepository;
+import manitto.backend.domain.group.repository.GroupTemplateRepository;
 import manitto.backend.global.util.PasswordProvider;
 import org.springframework.stereotype.Service;
 
@@ -16,17 +16,18 @@ import org.springframework.stereotype.Service;
 public class GroupService {
 
     private final GroupValidator groupValidator;
+    private final GroupTemplateRepository groupTemplateRepository;
     private final GroupCountTemplateRepository groupCountTemplateRepository;
-    private final GlobalMongoTemplateRepository globalMongoTemplateRepository;
 
     public GroupCreateRes create(GroupCreateReq req) {
 
         groupValidator.validateLeaderAndGroupUnique(req.getLeaderName(), req.getGroupName());
 
-        Group group = Group.create(req.getLeaderName(), req.getGroupName(), PasswordProvider.generatePassword());
-        group = globalMongoTemplateRepository.saveWithoutDuplicatedId(group, Group.class);
+        Group group = groupTemplateRepository.create(req.getLeaderName(), req.getGroupName(),
+                PasswordProvider.generatePassword());
 
-        return GroupDtoMapper.toGroupCreateRes(group.getId());
+        return GroupDtoMapper.toGroupCreateRes(group.getId(), group.getLeaderName(), group.getGroupName(),
+                group.getPassword());
     }
 
     public GroupCountRes count() {
